03399841b0bed52443ddeaccf1f2191d3c992f71
Daniel Shao <dsshao821@gmail.com>
2025-08-27 05:26:40 +0000

    ephemeral

---
+++ models/demo/clean_orders.sql
@@ -0,0 +1,19 @@
+{{
+    config
+    (
+        materialized = 'ephemeral'
+    )
+}}
+
+with base_orders as
+(
+    select
+      ORDER_ID
+    , ORDER_DATE
+    , CUSTOMER_ID
+    , CASE WHEN CUSTOMER_NAME IS NULL THEN 'NA' ELSE UPPER(CUSTOMER_NAME) END AS CUSTOMER_NAME
+    , CREATED_AT
+    from {{source('orders','BASE_ORDERS')}}
+    where order_date is not null
+)
+select * from base_orders
